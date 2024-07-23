44f5dae18fc07732fb3f168d6b7904c065e22728
JOSE EDUARDO SABILLON HURTADO <joseeduardosabillon157@gmail.com>
2024-07-23 16:51:39 -0600

    Add files via upload
    comit clase 23 de Julio

--- Personaje.py
+++ Personaje.py
@@ -4,14 +4,35 @@ import pygame
 import Constantes
 
 class Personaje():
-    def __init__(self, x, y):
+    def __init__(self, x, y, animaciones):
+        self.flip = False
+        self.animaciones = animaciones
+        #imagen de la animacion que se esta mostrando actualmente
+        self.frame_index = 0
+        #Aqui se almacena la hora actual (en milisegundos desde que se inicia)
+        self.update_time = pygame.time.get_ticks()
+        self.image = animaciones[self.frame_index]
         self.forma = pygame.Rect(0, 0, Constantes.ANCHO_PERSONAJE,
                                         Constantes.ALTO_PERSONAJE)
         self.forma.center = (x,y)
 
-    def dibujar(self, interfaz):
-        pygame.draw.rect(interfaz, (255, 250, 0),  self.forma)
+    def update(self):
+        cooldown_animacion = 100
+        self.image = self.animaciones[self.frame_index]
 
-    def movimineto(self, delta_x, delta_y):
-        self.forma.x = self.forma.x + delta_x
-        self.forma.y = self.forma.y + delta_y
+
+
+    def dibujar(self, interfaz):
+        imagen_flip = pygame.transform.flip(self.image, self.flip, False)
+        interfaz.blit(self.image, self.forma)
+      #  pygame.draw.rect(interfaz, (255, 250, 0),  self.forma, 1)
+
+
+def movimineto(self, delta_x, delta_y):
+    if delta_x < 0:
+        self.flip = True
+    if delta_x > 0:
+        self.flip = False
+
+    self.forma.x = self.forma.x + delta_x
+    self.forma.y = self.forma.y + delta_y
--- main.py
+++ main.py
@@ -2,20 +2,32 @@
 
 import pygame
 import Constantes
+import player
 from Personaje import Personaje
 
-
-Jugador = Personaje(20, 20)
-
 pygame.init()
-
-ancho = 800
-alto = 600
 
 ventana = pygame.display.set_mode((Constantes.ANCHO_VENTANA,
                                    Constantes.ALTO_VENTANA))
 
 pygame.display.set_caption("Mi Primer Juego")
+
+def escala_img(image, scala):
+    w = image.get_width()
+    h = image.get_height()
+    nueva_imagen = pygame.transform.scale(image, (w*scala, h*scala))
+    return nueva_imagen
+
+
+animaciones= []
+for i in range (6):
+    img = pygame.image.load(f"assets//images//characters//player//Player_{i}.png")
+    img =escala_img(img, Constantes.SCALA_PERSONAJE)
+    animaciones.append(img)
+
+
+Jugador = Personaje(50, 50, animaciones)
+
 
 #DEFINIR LAS VARIABLES DE MOVIMIENTO DEL JUGADOR
 mover_arriba = False
@@ -23,8 +35,15 @@ mover_abajo = False
 mover_izquierda = False
 mover_derecha = False
 
+#Controlar el freme red
+reloj = pygame.time.Clock()
+
 run = True
 while run == True:
+
+    #Que vaya a 60 fps
+    reloj.tick(Constantes.FPS)
+
 
     ventana.fill(Constantes.COLOR_BG)
 
@@ -33,13 +52,13 @@ while run == True:
     delta_y = 0
 
     if mover_derecha == True:
-        delta_x = 5
+        delta_x = Constantes.VELOCIDAD
     if mover_izquierda == True:
-        delta_x = -5
+        delta_x = -Constantes.VELOCIDAD
     if mover_arriba == True:
-        delta_x = -5
-    if mover_derecha == True:
-        delta_x = 5
+        delta_y = -Constantes.VELOCIDAD
+    if mover_abajo == True:
+        delta_y = Constantes.VELOCIDAD
 
     #Mover al Jugador
     Jugador.movimineto(delta_x, delta_y)
@@ -61,6 +80,16 @@ while run == True:
             if event.key == pygame.K_s:
                 mover_abajo = True
 
+        # PAra cuando se suelte la tecla
+    if event.type == pygame.KEYUP:
+        if event.key == pygame.K_a:
+            mover_izquierda = False
+        if event.key == pygame.K_d:
+            mover_derecha = False
+        if event.key == pygame.K_w:
+            mover_arriba = False
+        if event.key == pygame.K_s:
+            mover_abajo = False
 
     pygame.display.update()
 
