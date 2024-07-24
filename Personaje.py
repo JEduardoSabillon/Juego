f02eb1430c61e42ecefaedbc2ebcc5b6934fa03b
JOSE EDUARDO SABILLON HURTADO <joseeduardosabillon157@gmail.com>
2024-07-23 22:31:50 -0600

    Add files via upload
    trabajo en casa

--- Personaje.py
+++ Personaje.py
@@ -7,32 +7,35 @@ class Personaje():
     def __init__(self, x, y, animaciones):
         self.flip = False
         self.animaciones = animaciones
-        #imagen de la animacion que se esta mostrando actualmente
+        #IMAGEN DE LA ANIMACION QUE SE ESTA MOSTRANDO ACTUALMENTE
         self.frame_index = 0
-        #Aqui se almacena la hora actual (en milisegundos desde que se inicia)
+        #AQUI SE ALMACENA LA HORA ACTUAL EN MILISEGUNDOS DESDE QUE SE INICIO PYGAME
         self.update_time = pygame.time.get_ticks()
         self.image = animaciones[self.frame_index]
         self.forma = pygame.Rect(0, 0, Constantes.ANCHO_PERSONAJE,
                                         Constantes.ALTO_PERSONAJE)
         self.forma.center = (x,y)
 
+
+    def movimeinto(self, delta_x, delta_y):
+        if delta_x < 0:
+            self.flip = True
+        if delta_x > 0:
+            self.flip = False
+        self.forma.x = self.forma.x + delta_x
+        self.forma.y = self.forma.y + delta_y
+
     def update(self):
         cooldown_animacion = 100
         self.image = self.animaciones[self.frame_index]
-
+        if pygame.time.get_ticks() - self.update_time >= cooldown_animacion:
+            self.frame_index = self.frame_index + 1
+            self.update_time = pygame.time.get_ticks()
+        if self.frame_index >= len(self.animaciones):
+            self.frame_index = 0
 
 
     def dibujar(self, interfaz):
         imagen_flip = pygame.transform.flip(self.image, self.flip, False)
-        interfaz.blit(self.image, self.forma)
-      #  pygame.draw.rect(interfaz, (255, 250, 0),  self.forma, 1)
-
-
-def movimineto(self, delta_x, delta_y):
-    if delta_x < 0:
-        self.flip = True
-    if delta_x > 0:
-        self.flip = False
-
-    self.forma.x = self.forma.x + delta_x
-    self.forma.y = self.forma.y + delta_y
+        interfaz.blit(imagen_flip, self.forma)
+        #pygame.draw.rect(interfaz, Constantes.COLOR_PERSONAJE,  self.forma, 1)
--- main.py
+++ main.py
@@ -1,32 +1,31 @@
-#MAIN.PY
+#MAIN
 
 import pygame
 import Constantes
-import player
 from Personaje import Personaje
 
 pygame.init()
-
 ventana = pygame.display.set_mode((Constantes.ANCHO_VENTANA,
                                    Constantes.ALTO_VENTANA))
-
 pygame.display.set_caption("Mi Primer Juego")
 
-def escala_img(image, scala):
+def escalar_img(image, scale):
     w = image.get_width()
     h = image.get_height()
-    nueva_imagen = pygame.transform.scale(image, (w*scala, h*scala))
+    nueva_imagen = pygame.transform.scale(image, (w*scale, h*scale))
     return nueva_imagen
 
-
-animaciones= []
-for i in range (6):
+animaciones = []
+for i in range (7):
     img = pygame.image.load(f"assets//images//characters//player//Player_{i}.png")
-    img =escala_img(img, Constantes.SCALA_PERSONAJE)
+    img = escalar_img(img, Constantes.SCALA_PERSONAJE)
     animaciones.append(img)
 
 
+
+
 Jugador = Personaje(50, 50, animaciones)
+
 
 
 #DEFINIR LAS VARIABLES DE MOVIMIENTO DEL JUGADOR
@@ -35,15 +34,16 @@ mover_abajo = False
 mover_izquierda = False
 mover_derecha = False
 
-#Controlar el freme red
+#CONTROLAR EL FRENTE RATE
 reloj = pygame.time.Clock()
 
 run = True
 while run == True:
 
-    #Que vaya a 60 fps
-    reloj.tick(Constantes.FPS)
 
+
+    #QUE VAYA A 60 FPS
+    reloj.tick(Constantes.FPS)
 
     ventana.fill(Constantes.COLOR_BG)
 
@@ -60,9 +60,10 @@ while run == True:
     if mover_abajo == True:
         delta_y = Constantes.VELOCIDAD
 
-    #Mover al Jugador
-    Jugador.movimineto(delta_x, delta_y)
+    #MVOVER ALL JUGADOR
+    Jugador.movimeinto(delta_x, delta_y)
 
+    Jugador.update()
 
     Jugador.dibujar(ventana)
 
@@ -80,16 +81,17 @@ while run == True:
             if event.key == pygame.K_s:
                 mover_abajo = True
 
-        # PAra cuando se suelte la tecla
-    if event.type == pygame.KEYUP:
-        if event.key == pygame.K_a:
-            mover_izquierda = False
-        if event.key == pygame.K_d:
-            mover_derecha = False
-        if event.key == pygame.K_w:
-            mover_arriba = False
-        if event.key == pygame.K_s:
-            mover_abajo = False
+
+        #PARA CUANDO SE SUELTA LA TECLA
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                mover_izquierda = False
+            if event.key == pygame.K_d:
+                mover_derecha = False
+            if event.key == pygame.K_w:
+                mover_arriba = False
+            if event.key == pygame.K_s:
+                mover_abajo = False
 
     pygame.display.update()
 
